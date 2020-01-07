62bcdd86305c56ccd608de29863da4f63a3b045d
Emerson Ribeiro <emersonrjr03@gmail.com>
2020-01-07 15:31:47 -0300

    adding production information to environment file

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  apiUrl: 'https://taskme-app.herokuapp.com/',
+  whitelistedDomains: ['https://taskme-app.herokuapp.com', 'taskme-app.herokuapp.com'],
+  blacklistedRoutes: ['https://taskme-app.herokuapp.com/auth', 'https://taskme-app.herokuapp.com/auth']
 };
