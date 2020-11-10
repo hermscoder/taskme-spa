c56ee7709644b6ee6365e2d760925deebb52ec28
emerson.junior <emerson.junior@sankhya.com.br>
2020-11-10 09:02:29 -0300

    creating funcions to check state on the tasksomeone service and doing refactor

--- src/app/tasks/current-tasks/current-tasks.component.ts
+++ src/app/tasks/current-tasks/current-tasks.component.ts
@@ -1,57 +1,61 @@
-import { Component, OnInit } from '@angular/core';
-import { TaskSomeoneDetailsDTO } from '../../_models/TaskSomeoneDetailsDTO';
-import { TaskSomeoneService } from '../../_services/task-someone.service';
-import { AlertifyService } from '../../_services/alertify.service';
-import { Pageable } from 'src/app/_models/Pageable';
-import { PaginationInfo } from 'src/app/_models/PaginationInfo';
-import { ActivatedRoute } from '@angular/router';
+import {Component, OnInit} from '@angular/core';
+import {TaskSomeoneDetailsDTO} from '../../_models/TaskSomeoneDetailsDTO';
+import {TaskSomeoneService} from '../../_services/task-someone.service';
+import {AlertifyService} from '../../_services/alertify.service';
+import {Pageable} from 'src/app/_models/Pageable';
+import {PaginationInfo} from 'src/app/_models/PaginationInfo';
+import {ActivatedRoute} from '@angular/router';
 
 
 @Component({
-  selector: 'app-current-tasks',
-  templateUrl: './current-tasks.component.html',
-  styleUrls: ['./current-tasks.component.css']
+    selector: 'app-current-tasks',
+    templateUrl: './current-tasks.component.html',
+    styleUrls: ['./current-tasks.component.css']
 })
 export class CurrentTasksComponent implements OnInit {
-	currentTasks: TaskSomeoneDetailsDTO[];
-  constructor(private taskSomeoneService: TaskSomeoneService, private alertify: AlertifyService, private route: ActivatedRoute) { }
-	paginationInfo: PaginationInfo;
+    currentTasks: TaskSomeoneDetailsDTO[];
+
+    constructor(private taskSomeoneService: TaskSomeoneService, private alertify: AlertifyService, private route: ActivatedRoute) {
+    }
+
+    paginationInfo: PaginationInfo;
 
 
-  ngOnInit() {
-    this.route.data.subscribe(data => {
-      this.currentTasks = data['currentTasksList'].content;
-      this.paginationInfo = new PaginationInfo(data['currentTasksList']);
-    });
-  }
+    ngOnInit() {
+        this.route.data.subscribe(data => {
+            this.currentTasks = data['currentTasksList'].content;
+            this.paginationInfo = new PaginationInfo(data['currentTasksList']);
+        });
+    }
 
-  listWithPagination(pageable: Pageable) {
-    this.taskSomeoneService.listCurrentUserTasks(pageable).subscribe((page: any[]) => {
-      this.currentTasks = page['content'];
-      this.paginationInfo = new PaginationInfo(page);
-    }, error => {
-      this.alertify.error(error.message);
-    });
-  }
+    listWithPagination(pageable: Pageable) {
+        this.taskSomeoneService.listCurrentUserTasks(pageable).subscribe((page: any[]) => {
+            this.currentTasks = page['content'];
+            this.paginationInfo = new PaginationInfo(page);
+        }, error => {
+            this.alertify.error(error.message);
+        });
+    }
 
-  getRiskClass(task: TaskSomeoneDetailsDTO){
-  	if(!task.endDate) {
-  		return 'none';
-  	}
+    getRiskClass(task: TaskSomeoneDetailsDTO) {
+        if (!task.endDate) {
+            return 'none';
+        }
 
-		var dateDiff = this.datediff( new Date(task.endDate), new Date(task.createdOn) );
-  	if(dateDiff < 5) {
-  		return 'danger';
-  	} else if(dateDiff < 10){
-  		return 'attention';
-  	} else if(dateDiff < 15){
-  		return 'some';
-  	} else {
-  		return 'none';
-  	}
-  }
-  datediff(first, second): number {
-    return Math.abs(Math.round((second-first)/(1000*60*60*24)));
-	}
+        let dateDiff = this.datediff(new Date(task.endDate), new Date());
+        if (dateDiff < 5) {
+            return 'danger';
+        } else if (dateDiff < 10) {
+            return 'attention';
+        } else if (dateDiff < 15) {
+            return 'some';
+        } else {
+            return 'none';
+        }
+    }
+
+    datediff(first, second): number {
+        return Math.abs(Math.round((second - first) / (1000 * 60 * 60 * 24)));
+    }
 
 }
